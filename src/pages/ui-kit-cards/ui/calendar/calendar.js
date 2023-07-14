ad0919447527d28e883ec26c5a5687978c6f88ed
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2023-07-15 00:05:17 +0500

    added calendar

---
+++ src/pages/ui-kit-cards/ui/calendar/calendar.js
@@ -0,0 +1,34 @@
+import AirDatepicker from 'air-datepicker';
+
+function makeCalendar(element) {
+  new AirDatepicker(element, {
+    inline: true,
+    range: true,
+    multipleDatesSeparator: ' - ',
+    selectedDates: [new Date('2019-08-19'), new Date('2019-08-23')],
+    buttons: [
+      {
+        content() {
+          return 'очистить'
+        },
+        async onClick(dp) {
+          await dp.clear()
+          dp.$el.value = ''
+        }
+      },
+      {
+        content(dp) {
+          return 'применить'
+        },
+        onClick(dp) {
+        }
+      }
+    ],
+  })
+}
+
+const calendar = document.querySelector('.js-calendar');
+
+if (calendar !== undefined) {
+  makeCalendar(calendar)
+}
